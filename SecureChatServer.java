0278c5c171160714f736d3575719f9f5e30f4c79
David Arena <david@davidarena.com>
2017-04-05 19:56:04 -0400

    Add files via upload
    Implemented SecureConnection integration

--- SecureChatServer.java
+++ SecureChatServer.java
@@ -4,7 +4,6 @@ import java.util.Date;
 import java.io.File;
 import java.io.PrintWriter;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -12,8 +11,8 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
+import java.net.BindException;
 
-//import
 /**
  * An enumerated type where each member corresponds to the ordinal of the first byte of an incoming message.
  * These represent the different types of communication the user has with the server (e.g. sending a regular message, requesting public keys,
@@ -24,8 +23,7 @@ enum TypeOfData {
 }
 
 /**
- * The first revision of the SecureChatServer implementation. It is very incomplete and missing many features that will be included in the final
- * product, most notably the encrytion and  This is meant as a test or proof of concept.
+ * The SecureChatServer implementation. It currently contains most of the features necessary, such as encryption and nicknames.
  * 
  * @author David Arena
  */
@@ -33,6 +31,9 @@ public class SecureChatServer extends Thread {
 	private static ArrayList<User> users = new ArrayList<User>();
 	private static int port;
 	private static String directory;
+	/**
+	 * LOGGING IS GETTING A HUGE OVERHAUL: LOGGING CURRENTLY HAS ISSUES AND IS NOT TOP PRIORITY
+	 */
 	private static File logFile;
 	private static PrintWriter logger;
 	
@@ -66,7 +67,6 @@ public class SecureChatServer extends Thread {
 			directory = args[1];
 			
 			logFile = new File(directory + "/logs" + port + ".txt");
-			logger = null;
 			try {
 				logger = new PrintWriter(logFile);
 			}
@@ -74,17 +74,22 @@ public class SecureChatServer extends Thread {
 				System.out.println(new Date() + " " + "<ERROR>: Log file could not be created at the specified directory.");
 			}
 			
-			ServerSocket listener = new ServerSocket(port);
-			System.out.println(new Date() + " " + "<SERVER>: Server initialized on port " + port + ".");
-			
 			try {
-				while (true) {
-					new User(listener.accept()).start();
+				ServerSocket listener = new ServerSocket(port);
+				System.out.println(new Date() + " " + "<SERVER>: Server initialized on port " + port + ".");
+				
+				try {
+					while (true) {
+						new User(listener.accept()).start();
+					}
+				}
+				finally {
+					listener.close();
+					System.out.println(new Date() + " " + "<SERVER>: Server has been closed.");
 				}
 			}
-			finally {
-				listener.close();
-				System.out.println(new Date() + " " + "<SERVER>: Server has been closed.");
+			catch (BindException b) {
+				System.out.println(new Date() + " " + "<ERROR>: There is already a service that is using the specified port.");
 			}
 		}
 	}
@@ -95,11 +100,14 @@ public class SecureChatServer extends Thread {
 	  
 	public static class User extends Thread
 	{
+		private SecureConnection secureCon = new SecureConnection();
 		private Socket socket;
 		private String nickname;
+		private boolean alive;
 		private boolean nicknameSet;
 		private byte[] data;
 		private byte[] input;
+		private byte[] pubKey;
 		private TypeOfData type;
 		
 		/**
@@ -122,6 +130,7 @@ public class SecureChatServer extends Thread {
 		public void run() {
 			nicknameSet = false;
 			nickname = "no_nickname";
+			alive = true;
 			try {
 				users.add(this);
 				logger.println("(nickname) joined the server.");
@@ -157,7 +166,9 @@ public class SecureChatServer extends Thread {
 								
 							case DH_PUB_KEY: 
 								System.out.println("<SERVER>: " + nickname + " issued a public key request.");
-								//TODO implement SecureConnection
+								pubKey = secureCon.getPublicKey(data);
+								this.send(pubKey, TypeOfData.DH_PUB_KEY);
+								secureCon.processOtherPubKey(data);
 								break;
 								
 							case NICKNAME: 
@@ -167,7 +178,9 @@ public class SecureChatServer extends Thread {
 										System.out.println(new Date() + " " + "<SERVER>: " + nickname + " joined the server.");
 										logger.println(new Date() + " " + "<SERVER>: " + nickname + " joined the server.");
 										try {
-											this.send((nickname + " joined the server.").getBytes("UTF-8"), TypeOfData.SERVER_MESSAGE);
+											for (User u : users) {
+												u.send((nickname + " joined the server.").getBytes("UTF-8"), TypeOfData.SERVER_MESSAGE);
+											}
 										}
 										catch (UnsupportedEncodingException u) {
 											u.printStackTrace();
@@ -188,7 +201,9 @@ public class SecureChatServer extends Thread {
 										System.out.print(new Date() + " " + "<SERVER>: " + nickname);
 										logger.print(new Date() + " " + "<SERVER>: " + nickname);
 										try {
-											this.send((nickname + " updated their nickname to " + new String (data) + ".").getBytes("UTF-8"), TypeOfData.SERVER_MESSAGE);
+											for (User u : users) {
+												u.send((nickname + " updated their nickname to " + new String (data) + ".").getBytes("UTF-8"), TypeOfData.SERVER_MESSAGE);
+											}
 										}
 										catch (UnsupportedEncodingException u) {
 											u.printStackTrace();
@@ -215,6 +230,7 @@ public class SecureChatServer extends Thread {
 					}
 					catch (Exception i) {
 						if (i instanceof SocketException) {
+							this.alive = false;
 							users.remove(this);
 							System.out.println(new Date() + " " + "<SERVER>: " + nickname + " disconnected from the server (socket lost connection).");
 							logger.println(new Date() + " " + "<SERVER>: " + nickname + " disconnected from the server (socket lost connection).");
@@ -231,14 +247,16 @@ public class SecureChatServer extends Thread {
 				}
 			}
 			finally {
-				users.remove(this);
-				System.out.println(new Date() + " " + "<SERVER>: " + nickname + " disconnected from the server.");
-				logger.println(new Date() + " " + "<SERVER>: " + nickname + " disconnected from the server.");
-				try {
-					socket.close();
-				}
-				catch (IOException i) {
-					System.out.println(i.getMessage());
+				if (alive) {
+					users.remove(this);
+					System.out.println(new Date() + " " + "<SERVER>: " + nickname + " disconnected from the server.");
+					logger.println(new Date() + " " + "<SERVER>: " + nickname + " disconnected from the server.");
+					try {
+						socket.close();
+					}
+					catch (IOException i) {
+						System.out.println(i.getMessage());
+					}
 				}
 			}
 		}
